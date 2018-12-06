e8c78dde2282e2e7deaace46e8aa91153cd19164
miguel <miguel.contrerastorrejon@gmail.com>
2018-12-05 21:19:14 -0300

    ADD: ejercicio2 clase 7 2

---
+++ ejercicio2/ejercicio2.rb
@@ -0,0 +1,23 @@
+## Ejercicio 2: Arrays y strings
+#Dado el arreglo
+
+#~~~ruby
+nombres = ["Violeta", "Andino", "Clemente", "Javiera", "Paula", "Pia", "Ray"]
+#~~~
+
+#Se pide:
+
+#1. Extraer todos los elementos que excedan mas de 5 caracteres utilizando el método *.select*.
+new_array = nombres.select{ |e| e.length > 5 }
+puts new_array
+puts "fin ejercicio 1"
+#- Utilizando *.map* crear una arreglo con los nombres en minúscula.
+new_array = nombres.map { |e| e.downcase}
+puts new_array
+puts "fin ejercicio 2"
+#- Utilizando *.select* para crear un arreglo con todos los nombres que empiecen con P.
+new_array = nombres.select {|e| e.include? "P"}
+puts new_array
+
+#- Utilizando *.map* crear un arreglo único con la cantidad de letras que tiene cada nombre.
+#- Utilizando *.map* y *.gsub* eliminar las vocales de todos los nombres.
