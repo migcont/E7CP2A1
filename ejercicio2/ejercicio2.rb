6ce4bcc257091b425b097907eb405399429f28bf
miguel <miguel.contrerastorrejon@gmail.com>
2018-12-05 22:30:43 -0300

    ADD:Ejercicio2 Terminado

--- ejercicio2/ejercicio2.rb
+++ ejercicio2/ejercicio2.rb
@@ -18,6 +18,11 @@ puts "fin ejercicio 2"
 #- Utilizando *.select* para crear un arreglo con todos los nombres que empiecen con P.
 new_array = nombres.select {|e| e.include? "P"}
 puts new_array
-
+puts "fin ejercicio 3"
 #- Utilizando *.map* crear un arreglo único con la cantidad de letras que tiene cada nombre.
+new_array = nombres.map {|e| e.length}
+puts new_array
+puts "fin ejercicio 4"
 #- Utilizando *.map* y *.gsub* eliminar las vocales de todos los nombres.
+new_array = nombres.map { |e| e.gsub(/[aeiouAEIOU]/, '%')}
+puts new_array
